20f4444aae10b1e06cccda13b0605014a43b6d6c
zep1994 <snaxbags@protonmail.com>
2019-10-26 22:05:51 -0400

    add express

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http = require('http')
+const express = require('express')
+
+const app = express()
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-const server = http.createServer((req, res) => {
-    console.log("hello")
-}).listen(3000)
+app.get('/', (req, res) => {
+    res.send('<h1>Hello</h1>')
+})
+
+app.listen(3000)
